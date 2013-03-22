491c4542149f7ec693c025a886e3dac9d32ebdc1
Gino Lucero <glucero@gmail.com>
2013-03-21 21:08:34 -0600

    adjusting documentation

--- roflcopter.rb
+++ roflcopter.rb
@@ -1,12 +1,7 @@
 #!/usr/bin/env ruby
 
-# requires ruby 2.0
-
-# the roflcopter blade animations are created the same way you create a plasma
-# effect. (http://lodev.org/cgtutor/plasma.html)
-#
-# once the copter is created, white space is added to the end of each line and
-# the entire frame is rotated column-wise like a piano roll.
+raise 'I require Ruby 2.0!' unless RUBY_VERSION['2.0']
+raise 'I require OSX!' unless RUBY_PLATFORM['darwin']
 
 require 'curses'
 include Curses
@@ -16,7 +11,7 @@ curs_set 0
 
 begin
   frame = 0
-  sound = Process.spawn %|say -v Alex the rofl copter says #{'siff ' * 10000}|
+  sound = Process.spawn %|say -v alex the rofl copter says #{'siff ' * 10000}|
 
   while (frame += 1)
     [
@@ -29,18 +24,21 @@ begin
       %|            I     I            |,
       %|        --------------/        |
     ].each_with_index do |line, index|
+      # the roflcopter blade animations are created the same way you create a
+      # plasma effect. (http://lodev.org/cgtutor/plasma.html)
       5.times do |iteration|
         if index.zero?
           line.tr! iteration.to_s, %|ROFL:|[-(iteration + frame) % 5]
         end
       end
-
       4.times do |iteration|
         if (2..4) === index
           line.tr! iteration.to_s, %|L    |[(iteration + frame) % 4]
         end
       end
 
+      # once the copter is created, white space is added to the end of each
+      # line and the entire frame is rotated column-wise like a piano roll.
       setpos(5 + index, 0)
       copter = line + (' ' * (cols - 31))
       copter = copter.chars.rotate -(index + frame)
